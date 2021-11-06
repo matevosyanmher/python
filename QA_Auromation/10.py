576f6ae0b206e997cdfe1e0b0c668db1789bfc42
Mher <matevosyanmher@gmail.com>
2021-11-07 01:32:45 +0400

    Finish

---
+++ QA_Auromation/10.py
@@ -0,0 +1,8 @@
+# 10. Write a Python program to check whether a specified list is sorted or not
+sampleList_10 = [1, 2, 4, 6, 8, 10, 12, 14, -16, 17]
+
+# for i in range(len(sampleList_10) - 1):
+if all(sampleList_10[i] <= sampleList_10[i + 1] for i in range(len(sampleList_10) - 1)):
+    print("ok")
+else:
+    print("not")
---
+++ QA_Auromation/9.py
@@ -0,0 +1,3 @@
+# 09. Write a Python program to find the item with maximum occurrences in a given lisy
+salmpleList_9 = ["apple", "orange", "lemon", "apple", "apple", "banana"]
+print(max(salmpleList_9, key=salmpleList_9.count))
