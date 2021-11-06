328c1132cebcda2fc4580c969be9775027c92ac4
Mher <matevosyanmher@gmail.com>
2021-11-07 01:49:10 +0400

    Finish

---
+++ QA_Auromation/1.py
@@ -0,0 +1,3 @@
+# 01. Write a program to sum all the items in a list
+sampleList_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print("sum of items of the list is: ", sum(sampleList_1))
---
+++ QA_Auromation/2.py
@@ -0,0 +1,3 @@
+# 02. Write a program to get the largest number from a list
+sampleList_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print("Max value of the list is: ", max(sampleList_2))
