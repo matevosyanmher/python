d6d4c0490dd387b7c979260b6a275a8ad0387af2
mher matevosyan <matevosyanmher@gmail.com>
2021-11-11 11:47:10 +0400

    Combine files_updated

--- QA_Automation/Homework_4_Built-in/test.py
+++ QA_Automation/Homework_4_Built-in/test.py
@@ -1,12 +1,13 @@
 import os
 
-outfile = open("xxx.txt", "x")
+outfile = open("Homework_4_build_in.py", "x")
 for filename in os.listdir("."):
-    if filename != "xxx.txt" or filename.endswith(".py"):
+    if filename != "Homework_4_build_in.txt" or filename.endswith(".py"):
         with open(filename, 'r') as readfile:
             for line in readfile:
-                outfile.write(line + "\n")
+                outfile.write(line)
                 # print(os.path.join(os.listdir("."), filename))
+        outfile.write("\n")
         continue
     else:
         continue
