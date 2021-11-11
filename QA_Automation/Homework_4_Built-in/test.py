4e9741288297a309b668034c7791c3bc590d624c
mher matevosyan <matevosyanmher@gmail.com>
2021-11-11 11:52:40 +0400

    Combine files_updated

--- QA_Automation/Homework_4_Built-in/test.py
+++ QA_Automation/Homework_4_Built-in/test.py
@@ -2,7 +2,7 @@ import os
 
 outfile = open("Homework_4_build_in.py", "x")
 for filename in os.listdir("."):
-    if filename != "Homework_4_build_in.txt" or filename.endswith(".py"):
+    if filename.endswith(".py") or filename != "test.py":
         with open(filename, 'r') as readfile:
             for line in readfile:
                 outfile.write(line)
