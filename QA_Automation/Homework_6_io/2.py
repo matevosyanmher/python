45b0204f8a5f10c7a4bfdb18c1774cbe65b97642
matevosyanmher <matevosyanmher@gmail.com>
2021-11-13 00:35:26 +0400

    Homework_6_io

--- QA_Automation/Homework_6_io/2.py
+++ QA_Automation/Homework_6_io/2.py
@@ -1,1 +1,11 @@
 # 2. Write a program to find the longest words
+file = open("file_name.txt", "r")
+
+lst = list(file)
+newList = []
+for i in lst:
+    a = i.strip("\n")
+    newList.append(a)
+print(max(newList, key=len))
+
+
--- QA_Automation/Homework_6_io/5.py
+++ QA_Automation/Homework_6_io/5.py
@@ -1,1 +1,11 @@
 # 5. Write a Python program to generate 26 text files named A.txt, B.txt, and so on up to Z.txt
+import string
+import os
+
+if not os.path.exists("letters"):
+    os.makedirs("letters")
+letter = string.ascii_uppercase
+for letter in string.ascii_uppercase:
+    with open("C:\\Users\Mher\PycharmProjects\python\QA_Automation\Homework_6_io\letters\\" + letter + ".txt", "w") as f:
+        f.write(letter)
+        f.close
