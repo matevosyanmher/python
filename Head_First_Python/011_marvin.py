5fc832e696211d03132717b0117b99517bab56a6
matevosyanmher <matevosyanmher@gmail.com>
2021-11-15 00:55:25 +0400

    Head First-ը բերել եմ այստեղ

--- Head_First_Python/011_marvin.py
+++ Head_First_Python/011_marvin.py
@@ -2,3 +2,14 @@ paranoid_android = "Marvin"
 letters = list(paranoid_android)
 for i in letters:
     print("\t", i)
+
+paranoid_android = "Marvin, the paranoid android"
+letters = list(paranoid_android)
+for i in letters[:6]:
+    print("\t", i)
+print()
+for i in letters[-7:]:
+    print("\t" * 2, i)
+print()
+for i in letters[12:20]:
+    print("\t" * 3, i)
