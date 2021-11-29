6919e31fbf51b2269da78e3313c23f8b13077971
mher matevosyan <matevosyanmher@gmail.com>
2021-11-29 17:48:25 +0400

    Head First vsearch is in module

---
+++ Head_First_Python/mymodules/vsearch.py
@@ -0,0 +1,16 @@
+def search4vowels(word: str) -> set:
+    """Return any vowels found in a supplied word."""
+    vowels = set("aeiou")
+    return vowels.intersection(set(word))
+
+
+# print(search4vowels('galaxy'))
+
+
+def search4letters(phrase: str, letter: str = "aeiou") -> set:
+    """Return a set of letters found in phrase"""
+    return set(letter).intersection(set(phrase))
+
+
+# print(search4letters("galaxy", "yaz"))
+# search4letters(phrase="galaxy", letter="xyz")
