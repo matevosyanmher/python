a2513daa71e9c46328675853dd12a26f8e7bc29a
matevosyanmher <matevosyanmher@gmail.com>
2021-12-22 01:47:13 +0400

    file read - write

---
+++ Head_First_Python/018_fileReadWrite.py
@@ -0,0 +1,14 @@
+# todos = open('todos.txt', 'a')
+#
+# print('Put out the trash. ', file=todos)
+# print('Feed the cat. ', file=todos)
+# print('Prepare tax return. ', file=todos)
+
+# todos.close()
+
+tasks = open('todos.txt')
+
+for chore in tasks:
+    print(chore, end='')
+
+tasks.close()
