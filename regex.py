f1e53ad34afbbad2d41943415c1534e2f9a54007
matevosyanmher <matevosyanmher@gmail.com>
2021-12-22 00:02:58 +0400

    regex update, + pyxll

--- regex.py
+++ regex.py
@@ -19,20 +19,20 @@ def regexp(string):
 regexp("Համաձայն Հ/Ա Ա43770154, ա12345678901, 43770 15405 ա/թ. 03/11/21 (Online banking 321656, dd 12/099/2021)")
 
 
-# import re
-# from pyxll import xl_func
-#
-# @xl_func
-# def regexp(string):
-#     # string = "Համաձայն Հ/Ա Ա43770154, ա12345678901, 4377015405 ա/թ. 03/11/21 (Online banking 321656, dd 12/099/2021)"
-#     new_string = re.sub('\(.*\)', '()', string)
-#     regex = r'[Ա|ա|\s]\d(?: ?\d){3,9}'
-#
-#     matches = re.findall(regex, new_string)
-#
-#     output = []
-#     for match in matches:
-#         match = match.replace(" ", "", 1)
-#         output.append(match)
-#
-#     return output
+import re
+from pyxll import xl_func
+
+@xl_func
+def regexp(string):
+    # string = "Համաձայն Հ/Ա Ա43770154, ա12345678901, 4377015405 ա/թ. 03/11/21 (Online banking 321656, dd 12/099/2021)"
+    new_string = re.sub('\(.*\)', '()', string)
+    regex = r'[Ա|ա|\s]\d(?: ?\d){3,9}'
+
+    matches = re.findall(regex, new_string)
+
+    output = []
+    for match in matches:
+        match = match.replace(" ", "", 1)
+        output.append(match)
+
+    return output
